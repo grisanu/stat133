2dcc883e3f827a5925b131bdb04a557ef7858160
mattgrisanu <matt.jgrisanu@berkeley.edu>
2014-10-21 16:47:37 -0700

    abline???

--- midterm/midterm.R
+++ midterm/midterm.R
@@ -12,38 +12,40 @@
 
 ########################################### FIRST PART
 #### We will start with a few small tasks.
+present = T;
 
 ## In one R command, create a variable [[ x ]] which is a vector and has
 ## all the even numbers between 2 and 20, in increasing order.
-
+x = seq(2,20,2);
 
 ## In one R command, create a variable [[ y ]] which is a vector and has
 ## all the odd numbers between -1 and -19, in decreasing order
-
+y = seq(-1,-19,-2);
 
 ## In one R command, create a variable [[ z ]] which is a vector and has
 ## the numbers 1 through 5, each repeated 4 times, in order (so 1 1 1 1 2 2 2 2 etc.)
-
+z = c(rep(1,4), rep(2,4), rep(3,4), rep(4,4), rep(5,4));
 
 ## In one R command, create a variable [[ m ]] that is a 3-by-4 matrix and has
 ## the entries 10, 20, through 120, where the numbers are entered row by row
 ## (so the first row is [ 10 20 30 40 ]).
-
+m = matrix(seq(10,120,10), nrow = 3, ncol = 4, byrow = T);
 
 ## Create a variable [[ n ]] that is a data frame of dimension 10 x 3
 ## where the first column is the numbers 1-10, the second column the letters a-j and the
 ## third column has T/F, T if the number in column 1 is even, F if the number in column 1 is odd.
 ## Then add the column names :  num, lett, even
-
+n = data.frame("num" = 1:10, "lett" = letters[1:10], "even" = c(F,T));
 
 ## Create a variable [[ p ]] that is a list and whose elements are x, y, z and m from above.
-
+p = list(x,y,z,m);
 
 ## Create a variable [[ nmat ]] that is a matrix of size 100 x 100 and whose
 ## elements are drawn from a Normal distribution with mean 6 and sd 2
 ## Do not remove the set.seed command
 set.seed(42)
 
+nmat = matrix(rnorm(100*100, mean = 6, sd = 2), nrow = 100, ncol = 100); ##
 
 ########################################### SECOND PART
 
@@ -52,25 +54,32 @@ set.seed(42)
 load("rainfallCO.rda")
 
 ## Extract the type of the variable rain and store in [[ type.rain ]]
-
+type.rain = class(rain);
 
 ## Extract the names of the elements in rain and store in  [[ name.stations ]]
-
+name.stations = names(rain);
 
 ## In one R command create the variable [[ n.obs ]] , a vector which stores the number
 ## of observations at EACH of the stations in rain
-
+n.obs = sapply(rain, length);
 
 ## In one R command create the variable [[ max.rain ]] which stores the maximum
 ## rainfall for each weather station.
+max.rain = sapply(rain, max);
 
 ## Plot a boxplot that shows the distribution of rainfall at each station
 ## (hint: there should be 5 "boxes" in the boxplot)
+boxplot(rain); ##
 
 ## Make a scatterplot that shows the rainfall at station 1 in year 2000.
 ## Precipitation should go on the y-axis and day on the x-axis
 ## Make new labels for the x- and y-axis.
 ## Hint: the date information is stored in the object day.
+in_year_2000 = which(day[[1]] >= 2000 & day[[1]] < 2001) ##
+
+plot(day[[1]][in_year_2000], rain[[1]][in_year_2000],
+     ylab = "Precipitation",
+     xlab = "Day")
 
 ########################################### THIRD PART
 #### Load in the infant data
@@ -85,18 +94,26 @@ fit <- lm(bwt ~ gestation, data=infants)
 ## Do a scatterplot of birthweight (on y-axis) vs. gestation (on x-axis)
 ## Add to the plot the line estimated in fit (hint: find the coefficients)
 ## Plot the line in red and put your own x- and y-axis labels on the plot.
+plot(infants$gestation, infants$bwt,
+     xlab = "gestation",
+     ylab = "bwt",
+     xlim = c(0,400),
+     ylim = c(0, 200))
 
-
+abline(coef=c(fit[[1]][1],fit[[2]][2]), col = "red") ###
 ## Plot a histogram of the fathers' heights (varible dht in the data frame).
+hist(infants$dht)
 
 ## Create a table which tallies the education level of mothers (varible ed in the data frame)
 ## against the education level of the fathers (variable ded in the data frame)
 
+ed.table = table(infants$ed, infants$ded);
 
 ########################################### FOURTH PART
 ## Write a function that takes as input a vector x and returns a standardized vector
 ## (i.e. x after subtracting the mean and dividing with the sd)
 
 standardize <- function(x){
-
+    stdizing = (x - mean(x))/sd(x);
+  return(stdizing)
 }
